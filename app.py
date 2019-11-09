cfd1ba00ee4c6d5fc3aaf3b5244116abadbbbd40
jack966 <54770362+jack966@users.noreply.github.com>
2019-11-10 00:08:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, url_for, json
+from databasetool import *
 
 app = Flask(__name__)
 
-@app.route('/page1/')
-def page1():
-    return render_template("page1.html")
 
-@app.route('/page2/')
-def page2():
-    return render_template("page2.html")
-
-@app.route('/page3/')
-def page3():
-    return render_template("page3.html")
-
-@app.route('/page4/')
-def page4():
-    return render_template("page4.html")
-
-@app.route('/page5/')
-def page5():
-    return render_template("page5.html")
-
-@app.route('/page6/')
-def page6():
-    return render_template("page6.html")
+@app.route('/', methods=['post', 'get'])
+def result():
+    qd_name = search_team_name()
+    return render_template("plays_compare.html", data1=qd_name, length1=len(qd_name),data2="",length2=0,data3="",length3=0)
 
 
-@app.route('/team/')
-def team():
-    return render_template("teams.htm")
-
-@app.route('/')
-def index():
-    return render_template("player.html")
-
-
+@app.route('/page1', methods=['GET'])
+def result1():
+    value1 = request.values.get('qd_msg1')
+    value2 = request.values.get('qd_msg2')
+    qd_name = search_team_name()
+    print(value1,value2)
+    qy_name1 = search_name_from_team(value1)
+    qy_name2 =search_name_from_team(value2)
+    return render_template("plays_compare.html", data1=qd_name, length1=len(qd_name),data2=qy_name1,length2=len(qy_name1),data3=qy_name2,length3=len(qy_name2))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
